32642ca993dc17d964a01ec175a6b4adc71d6c5d
P4mm <100779250+P4mm@users.noreply.github.com>
2022-04-17 19:26:23 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,37 @@
+var valorSorteado = 0
+var chances = 3
+
+randomGenerator();
+
+function chutar(){
+  var chute = document.getElementById("chute").value
+  var elementoResposta = document.getElementById("resposta")
+  
+  if ((parseInt(chute) == valorSorteado) && (chances > 0)){
+    elementoResposta.innerHTML = "👏🏻Acertou, este era realmente o número"
+  }else if((parseInt(chute) > valorSorteado) && (chances > 0)){
+    elementoResposta.innerHTML = "😔Errou, o número é mais baixo"
+    chances -= 1
+  }else if((parseInt(chute) < valorSorteado) && (chances > 0)){
+    elementoResposta.innerHTML = "😔Errou, o número é mais alto"
+    chances -= 1
+  }else if(parseInt(chances) == 0){
+    elementoResposta.innerHTML = "😔Que pena, suas chances acabaram, mais sorte da próxima vez"
+  }else if(chute == ""){
+    elementoResposta.innerHTML = "Insira algum número"
+  }else{
+    elementoResposta.innerHTML = "Houve um erro inesperado, por favor recarregue a página"
+    console.error("Erro inesperado, nenhuma das condições foi atendida, verificar código")
+  }
+  
+  event.preventDefault();
+}
+
+function cleanForm(){
+  document.getElementById("form").innerHTML = ""
+  
+}
+
+function randomGenerator(){
+  valorSorteado = Math.floor(Math.random() * 11);
+}
